5ec64568816c872ae6206118f7ed2d3515ae6b01
Samuel Cohen <sc.sketch@gmail.com>
2023-11-18 00:38:31 -0800

    game loop working with entry and results

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use colored::Colorize;
+use rand::distributions::{Distribution, Uniform};
+use rand::Rng;
 use std::error;
 use std::fmt;
 use std::io;
+use std::time::{Duration, Instant};
 
 struct AppError;
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+const UNICODE_CHECKMARK: &str = "\u{2714}";
+const UNICODE_X: &str = "\u{0078}";
 
 impl fmt::Display for AppError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -40,7 +47,7 @@ impl Multiplication {
 
 impl Problem for Multiplication {
     fn get_question(&self) -> String {
-        format!("{} * {} = ", self.first_digit, self.second_digit)
+        format!("{} * {}", self.first_digit, self.second_digit)
     }
 
     fn get_question_with_solution(&self) -> String {
@@ -64,48 +71,113 @@ impl fmt::Display for Multiplication {
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Let's do some math!");
 
-    let mut problems = vec![];
-    let _ = generate_problems(&mut problems).map_err(|e| {
-        println!("Error generating problems: {e:?}");
-    });
-
-    let mut buf = String::new();
-    for problem in problems.clone() {
-        let solution = problem.get_solution().to_string();
-
-        println!("{}", problem.get_question());
-
+    loop {
+        println!("How may problems do you want to do?");
+        let mut num_problems = String::new();
         io::stdin()
-            .read_line(&mut buf)
-            .expect("Failed to read line.");
+            .read_line(&mut num_problems)
+            .expect("Invalid input");
 
-        let input = buf.trim().to_lowercase();
+        let num_problems = num_problems
+            .trim()
+            .parse::<i32>()
+            .expect("invalid number of problems entered");
 
-        match input.as_str() {
-            "exit" => break,
-            _ => {
-                if solution == input {
-                    println!("SOLVED IT!");
-                } else {
-                    println!("INCORRECT!");
+        let mut problems = vec![];
+        let _ = generate_problems(&mut problems, num_problems).map_err(|e| {
+            println!("Error generating problems: {e:?}");
+        });
+
+        let mut answers = vec![];
+        let start_time = std::time::Instant::now();
+
+        let mut num_correct = 0;
+        let mut num_attempted = 0;
+        let mut buf = String::new();
+        for (index, problem) in problems.iter().enumerate() {
+            let solution = problem.get_solution().to_string();
+
+            println!("\nQuestion {} out of {}", (index + 1), num_problems);
+            println!("{} = ", problem.get_question());
+
+            io::stdin()
+                .read_line(&mut buf)
+                .expect("Failed to read line.");
+
+            let input = buf.trim().to_lowercase();
+
+            match input.trim().to_lowercase().as_str() {
+                "exit" => break,
+                _ => {
+                    // TODO: validate bad input
+                    if solution == input {
+                        num_correct += 1;
+                    }
+
+                    answers.push(input);
+
+                    num_attempted += 1;
                 }
+            }
+
+            buf.clear();
+        }
+
+        let elapsed = (Instant::now() - start_time).as_secs_f32();
+
+        for i in 0..num_attempted {
+            let problem = problems.get(i).expect("invalid problem");
+            let solution = problem.get_solution().to_string();
+            let answer = &answers[i];
+            if &solution == answer {
+                println!(
+                    "{} {}",
+                    UNICODE_CHECKMARK.green(),
+                    problem.get_question_with_solution()
+                );
+            } else {
+                println!(
+                    "{} {} = {} ({})",
+                    UNICODE_X.red(),
+                    problem.get_question(),
+                    answer.red(),
+                    solution.green()
+                );
             }
         }
 
-        buf.clear();
+        let percent = ((num_correct as f32 / num_attempted as f32) * 100.0).floor();
+        println!(
+            "\nYou got {} out of {} ({}%) in {:.2} seconds",
+            num_correct, num_attempted, percent, elapsed
+        );
+
+        let mut continue_buf = String::new();
+        println!("Play again? (Y/N)");
+        io::stdin()
+            .read_line(&mut continue_buf)
+            .expect("Invalid input");
+
+        match continue_buf.trim().to_lowercase().as_str() {
+            "y" => {}
+            _ => break,
+        };
     }
 }
 
-fn generate_problems(problems: &mut Vec<Multiplication>) -> Result<()> {
-    let mut new_problems = vec![
-        Multiplication::new(2, 3),
-        Multiplication::new(4, 2),
-        Multiplication::new(8, 7),
-    ];
+fn generate_problems(problems: &mut Vec<Multiplication>, count: i32) -> Result<()> {
+    let mut rng = rand::thread_rng();
+    let uniform = Uniform::from(1..=12);
 
-    problems.append(&mut new_problems);
+    for n in 0..count {
+        let first_digit = uniform.sample(&mut rng);
+        let second_digit = uniform.sample(&mut rng);
+        let problem = Multiplication::new(first_digit, second_digit);
+
+        problems.push(problem);
+    }
 
     Ok(())
 }
